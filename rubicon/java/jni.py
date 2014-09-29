1d4b71559b7a31cf3c3c38a34ef00d469f686bc1
Russell Keith-Magee <russell@keith-magee.com>
2014-09-29 10:02:08 +0800

    Fixed problems in JNI declarations.

--- rubicon/java/jni.py
+++ rubicon/java/jni.py
@@ -1,9 +1,12 @@
 from ctypes import *
 from ctypes import util
+import os
 
 from .types import *
 
-java = cdll.LoadLibrary(util.find_library('rubicon'))
+# If we're on Android, the SO file isn't on the LD_LIBRARY_PATH,
+# so we have to manually specify it.
+java = cdll.LoadLibrary(os.environ.get('RUBICON_LIBRARY', util.find_library('rubicon')))
 
 java.GetVersion.restype = jint
 java.GetVersion.argtypes = []
@@ -34,11 +37,11 @@ java.ThrowNew.restype = jint
 java.ThrowNew.argtypes = [jclass, c_char_p]
 java.ExceptionOccurred.restype = jthrowable
 java.ExceptionOccurred.argtypes = []
-java.ExceptionDescribe.restype = void
+java.ExceptionDescribe.restype = None
 java.ExceptionDescribe.argtypes = []
-java.ExceptionClear.restype = void
+java.ExceptionClear.restype = None
 java.ExceptionClear.argtypes = []
-java.FatalError.restype = void
+java.FatalError.restype = None
 java.FatalError.argtypes = [c_char_p]
 
 java.PushLocalFrame.restype = jint
@@ -48,9 +51,9 @@ java.PopLocalFrame.argtypes = [jobject]
 
 java.NewGlobalRef.restype = jobject
 java.NewGlobalRef.argtypes = [jobject]
-java.DeleteGlobalRef.restype = void
+java.DeleteGlobalRef.restype = None
 java.DeleteGlobalRef.argtypes = [jobject]
-java.DeleteLocalRef.restype = void
+java.DeleteLocalRef.restype = None
 java.DeleteLocalRef.argtypes = [jobject]
 
 java.IsSameObject.restype = jboolean
@@ -92,7 +95,7 @@ java.CallFloatMethod.restype = jfloat
 java.CallFloatMethod.argtypes = [jobject, jmethodID]
 java.CallDoubleMethod.restype = jdouble
 java.CallDoubleMethod.argtypes = [jobject, jmethodID]
-java.CallVoidMethod.restype = void
+java.CallVoidMethod.restype = None
 java.CallVoidMethod.argtypes = [jobject, jmethodID]
 
 java.CallNonvirtualObjectMethod.restype = jobject
@@ -113,7 +116,7 @@ java.CallNonvirtualFloatMethod.restype = jfloat
 java.CallNonvirtualFloatMethod.argtypes = [jobject, jclass, jmethodID]
 java.CallNonvirtualDoubleMethod.restype = jdouble
 java.CallNonvirtualDoubleMethod.argtypes = [jobject, jclass, jmethodID]
-java.CallNonvirtualVoidMethod.restype = void
+java.CallNonvirtualVoidMethod.restype = None
 java.CallNonvirtualVoidMethod.argtypes = [jobject, jclass, jmethodID]
 
 java.GetFieldID.restype = jfieldID
@@ -138,23 +141,23 @@ java.GetFloatField.argtypes = [jobject, jfieldID]
 java.GetDoubleField.restype = jdouble
 java.GetDoubleField.argtypes = [jobject, jfieldID]
 
-java.SetObjectField.restype = void
+java.SetObjectField.restype = None
 java.SetObjectField.argtypes = [jobject, jfieldID, jobject]
-java.SetBooleanField.restype = void
+java.SetBooleanField.restype = None
 java.SetBooleanField.argtypes = [jobject, jfieldID, jboolean]
-java.SetByteField.restype = void
+java.SetByteField.restype = None
 java.SetByteField.argtypes = [jobject, jfieldID, jbyte]
-java.SetCharField.restype = void
+java.SetCharField.restype = None
 java.SetCharField.argtypes = [jobject, jfieldID, jchar]
-java.SetShortField.restype = void
+java.SetShortField.restype = None
 java.SetShortField.argtypes = [jobject, jfieldID, jshort]
-java.SetIntField.restype = void
+java.SetIntField.restype = None
 java.SetIntField.argtypes = [jobject, jfieldID, jint]
-java.SetLongField.restype = void
+java.SetLongField.restype = None
 java.SetLongField.argtypes = [jobject, jfieldID, jlong]
-java.SetFloatField.restype = void
+java.SetFloatField.restype = None
 java.SetFloatField.argtypes = [jobject, jfieldID, jfloat]
-java.SetDoubleField.restype = void
+java.SetDoubleField.restype = None
 java.SetDoubleField.argtypes = [jobject, jfieldID, jdouble]
 
 java.GetStaticMethodID.restype = jmethodID
@@ -178,7 +181,7 @@ java.CallStaticFloatMethod.restype = jfloat
 java.CallStaticFloatMethod.argtypes = [jclass, jmethodID]
 java.CallStaticDoubleMethod.restype = jdouble
 java.CallStaticDoubleMethod.argtypes = [jclass, jmethodID]
-java.CallStaticVoidMethod.restype = void
+java.CallStaticVoidMethod.restype = None
 java.CallStaticVoidMethod.argtypes = [jclass, jmethodID]
 
 java.GetStaticFieldID.restype = jfieldID
@@ -203,23 +206,23 @@ java.GetStaticFloatField.argtypes = [jclass, jfieldID]
 java.GetStaticDoubleField.restype = jdouble
 java.GetStaticDoubleField.argtypes = [jclass, jfieldID]
 
-java.SetStaticObjectField.restype = void
+java.SetStaticObjectField.restype = None
 java.SetStaticObjectField.argtypes = [jclass, jfieldID, jobject]
-java.SetStaticBooleanField.restype = void
+java.SetStaticBooleanField.restype = None
 java.SetStaticBooleanField.argtypes = [jclass, jfieldID, jboolean]
-java.SetStaticByteField.restype = void
+java.SetStaticByteField.restype = None
 java.SetStaticByteField.argtypes = [jclass, jfieldID, jbyte]
-java.SetStaticCharField.restype = void
+java.SetStaticCharField.restype = None
 java.SetStaticCharField.argtypes = [jclass, jfieldID, jchar]
-java.SetStaticShortField.restype = void
+java.SetStaticShortField.restype = None
 java.SetStaticShortField.argtypes = [jclass, jfieldID, jshort]
-java.SetStaticIntField.restype = void
+java.SetStaticIntField.restype = None
 java.SetStaticIntField.argtypes = [jclass, jfieldID, jint]
-java.SetStaticLongField.restype = void
+java.SetStaticLongField.restype = None
 java.SetStaticLongField.argtypes = [jclass, jfieldID, jlong]
-java.SetStaticFloatField.restype = void
+java.SetStaticFloatField.restype = None
 java.SetStaticFloatField.argtypes = [jclass, jfieldID, jfloat]
-java.SetStaticDoubleField.restype = void
+java.SetStaticDoubleField.restype = None
 java.SetStaticDoubleField.argtypes = [jclass, jfieldID, jdouble]
 
 java.NewString.restype = jstring
@@ -228,7 +231,7 @@ java.GetStringLength.restype = jsize
 java.GetStringLength.argtypes = [jstring]
 java.GetStringChars.restype = jchar_p
 java.GetStringChars.argtypes = [jstring, jboolean_p]
-java.ReleaseStringChars.restype = void
+java.ReleaseStringChars.restype = None
 java.ReleaseStringChars.argtypes = [jstring, jchar_p]
 
 java.NewStringUTF.restype = jstring
@@ -237,7 +240,7 @@ java.GetStringUTFLength.restype = jsize
 java.GetStringUTFLength.argtypes = [jstring]
 java.GetStringUTFChars.restype = c_char_p
 java.GetStringUTFChars.argtypes = [jstring, jboolean_p]
-java.ReleaseStringUTFChars.restype = void
+java.ReleaseStringUTFChars.restype = None
 java.ReleaseStringUTFChars.argtypes = [jstring, c_char_p]
 
 java.GetArrayLength.restype = jsize
@@ -246,7 +249,7 @@ java.NewObjectArray.restype = jobjectArray
 java.NewObjectArray.argtypes = [jsize, jclass, jobject]
 java.GetObjectArrayElement.restype = jobject
 java.GetObjectArrayElement.argtypes = [jobjectArray, jsize]
-java.SetObjectArrayElement.restype = void
+java.SetObjectArrayElement.restype = None
 java.SetObjectArrayElement.argtypes = [jobjectArray, jsize, jobject]
 
 java.NewBooleanArray.restype = jbooleanArray
@@ -283,57 +286,58 @@ java.GetFloatArrayElements.argtypes = [jfloatArray, jboolean_p]
 java.GetDoubleArrayElements.restype = jdouble_p
 java.GetDoubleArrayElements.argtypes = [jdoubleArray, jboolean_p]
 
-java.ReleaseBooleanArrayElements.restype = void
+java.ReleaseBooleanArrayElements.restype = None
 java.ReleaseBooleanArrayElements.argtypes = [jbooleanArray, jboolean_p, jint]
-java.ReleaseByteArrayElements.restype = void
+java.ReleaseByteArrayElements.restype = None
 java.ReleaseByteArrayElements.argtypes = [jbyteArray, jbyte_p, jint]
-java.ReleaseCharArrayElements.restype = void
+java.ReleaseCharArrayElements.restype = None
 java.ReleaseCharArrayElements.argtypes = [jcharArray, jchar_p, jint]
-java.ReleaseShortArrayElements.restype = void
+java.ReleaseShortArrayElements.restype = None
 java.ReleaseShortArrayElements.argtypes = [jshortArray, jshort_p, jint]
-java.ReleaseIntArrayElements.restype = void
+java.ReleaseIntArrayElements.restype = None
 java.ReleaseIntArrayElements.argtypes = [jintArray, jint_p, jint]
-java.ReleaseLongArrayElements.restype = void
+java.ReleaseLongArrayElements.restype = None
 java.ReleaseLongArrayElements.argtypes = [jlongArray, jlong_p, jint]
-java.ReleaseFloatArrayElements.restype = void
+java.ReleaseFloatArrayElements.restype = None
 java.ReleaseFloatArrayElements.argtypes = [jfloatArray, jfloat_p, jint]
-java.ReleaseDoubleArrayElements.restype = void
+java.ReleaseDoubleArrayElements.restype = None
 java.ReleaseDoubleArrayElements.argtypes = [jdoubleArray, jdouble_p, jint]
 
-java.GetBooleanArrayRegion.restype = void
+java.GetBooleanArrayRegion.restype = None
 java.GetBooleanArrayRegion.argtypes = [jbooleanArray, jsize, jsize, jboolean_p]
-java.GetByteArrayRegion.restype = void
+java.GetByteArrayRegion.restype = None
 java.GetByteArrayRegion.argtypes = [jbyteArray, jsize, jsize, jbyte_p]
-java.GetCharArrayRegion.restype = void
+java.GetCharArrayRegion.restype = None
 java.GetCharArrayRegion.argtypes = [jcharArray, jsize, jsize, jchar_p]
-java.GetShortArrayRegion.restype = void
+java.GetShortArrayRegion.restype = None
 java.GetShortArrayRegion.argtypes = [jshortArray, jsize, jsize, jshort_p]
-java.GetIntArrayRegion.restype = void
+java.GetIntArrayRegion.restype = None
 java.GetIntArrayRegion.argtypes = [jintArray, jsize, jsize, jint_p]
-java.GetLongArrayRegion.restype = void
+java.GetLongArrayRegion.restype = None
 java.GetLongArrayRegion.argtypes = [jlongArray, jsize, jsize, jlong_p]
-java.GetFloatArrayRegion.restype = void
+java.GetFloatArrayRegion.restype = None
 java.GetFloatArrayRegion.argtypes = [jfloatArray, jsize, jsize, jfloat_p]
-java.GetDoubleArrayRegion.restype = void
+java.GetDoubleArrayRegion.restype = None
 java.GetDoubleArrayRegion.argtypes = [jdoubleArray, jsize, jsize, jdouble_p]
 
-java.SetBooleanArrayRegion.restype = void
+java.SetBooleanArrayRegion.restype = None
 java.SetBooleanArrayRegion.argtypes = [jbooleanArray, jsize, jsize, jboolean_p]
-java.SetByteArrayRegion.restype = void
+java.SetByteArrayRegion.restype = None
 java.SetByteArrayRegion.argtypes = [jbyteArray, jsize, jsize, jbyte_p]
-java.SetCharArrayRegion.restype = void
+java.SetCharArrayRegion.restype = None
 java.SetCharArrayRegion.argtypes = [jcharArray, jsize, jsize, jchar_p]
-java.SetShortArrayRegion.restype = void
+java.SetShortArrayRegion.restype = None
 java.SetShortArrayRegion.argtypes = [jshortArray, jsize, jsize, jshort_p]
-java.SetIntArrayRegion.restype = void
+java.SetIntArrayRegion.restype = None
 java.SetIntArrayRegion.argtypes = [jintArray, jsize, jsize, jint_p]
-java.SetLongArrayRegion.restype = void
+java.SetLongArrayRegion.restype = None
 java.SetLongArrayRegion.argtypes = [jlongArray, jsize, jsize, jlong_p]
-java.SetFloatArrayRegion.restype = void
+java.SetFloatArrayRegion.restype = None
 java.SetFloatArrayRegion.argtypes = [jfloatArray, jsize, jsize, jfloat_p]
-java.SetDoubleArrayRegion.restype = void
+java.SetDoubleArrayRegion.restype = None
 java.SetDoubleArrayRegion.argtypes = [jdoubleArray, jsize, jsize, jdouble_p]
 
+# FIXME
 # java.RegisterNatives.restype = jint
 # java.RegisterNatives.argtypes = [jclass, const JNINativeMethod *methods, jint]
 java.UnregisterNatives.restype = jint
@@ -344,28 +348,30 @@ java.MonitorEnter.argtypes = [jobject]
 java.MonitorExit.restype = jint
 java.MonitorExit.argtypes = [jobject]
 
-java.GetJavaVM.restype = jint
-java.GetJavaVM.argtypes = [JavaVM **vm]
+# FIXME
+# java.GetJavaVM.restype = jint
+# java.GetJavaVM.argtypes = [JavaVM **vm]
 
-java.GetStringRegion.restype = void
-java.GetStringRegion.argtypes = [jstring, jsize, jsize, jchar *buf]
-java.GetStringUTFRegion.restype = void
-java.GetStringUTFRegion.argtypes = [jstring, jsize, jsize, char *buf]
+java.GetStringRegion.restype = None
+java.GetStringRegion.argtypes = [jstring, jsize, jsize, jchar_p]
+java.GetStringUTFRegion.restype = None
+java.GetStringUTFRegion.argtypes = [jstring, jsize, jsize, c_char_p]
 
 java.GetPrimitiveArrayCritical.restype = c_void_p
 java.GetPrimitiveArrayCritical.argtypes = [jarray, jboolean_p]
-java.ReleasePrimitiveArrayCritical.restype = void
+java.ReleasePrimitiveArrayCritical.restype = None
 java.ReleasePrimitiveArrayCritical.argtypes = [jarray, c_void_p, jint]
 
 java.GetStringCritical.restype = jchar_p
 java.GetStringCritical.argtypes = [jstring, jboolean_p]
-java.ReleaseStringCritical.restype = void
+java.ReleaseStringCritical.restype = None
 java.ReleaseStringCritical.argtypes = [jstring, jchar_p]
 
-java.NewWeakGlobalRef.restype = jweak
-java.NewWeakGlobalRef.argtypes = [jobject]
-java.DeleteWeakGlobalRef.restype = void
-java.DeleteWeakGlobalRef.argtypes = [jweak]
+# FIXME
+# java.NewWeakGlobalRef.restype = jweak
+# java.NewWeakGlobalRef.argtypes = [jobject]
+# java.DeleteWeakGlobalRef.restype = None
+# java.DeleteWeakGlobalRef.argtypes = [jweak]
 
 java.ExceptionCheck.restype = jboolean
 java.ExceptionCheck.argtypes = []
@@ -377,5 +383,6 @@ java.GetDirectBufferAddress.argtypes = [jobject]
 java.GetDirectBufferCapacity.restype = jlong
 java.GetDirectBufferCapacity.argtypes = [jobject]
 
-java.GetObjectRefType.restype = jobjectRefType
-java.GetObjectRefType.argtypes = [jobject]
+# FIXME
+# java.GetObjectRefType.restype = jobjectRefType
+# java.GetObjectRefType.argtypes = [jobject]
